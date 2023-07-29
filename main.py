78228cf6a180e5ea7041c74ef90fae327c0d155d
SubhashSrinivasa <apssrini09@gmail.com>
2023-07-29 15:41:53 -0700

    a

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 
-
 loaded_model = tf.keras.models.load_model('Neural_Network/diabetes_identifier.h5')
 
-userInputtedData = [[5, 111, 73, 26, 0, 38, 0.548, 62]]
-scaler = StandardScaler()
-userInputtedData = scaler.fit(userInputtedData)
+userInputtedData = [[1,85,66,29,0,26.6,0.351,31]]
+# scaler = StandardScaler()
+# userInputtedData = scaler.fit_transform(userInputtedData)
 
-predictions = loaded_model.predict(userInputtedData)
+percentprob = loaded_model.predict(userInputtedData)[0][0] * 100
+if percentprob >= 50.0:
+    prediction = 'Diabetes Positive'
+else:
+    prediction = 'Diabetes Negative'
 
-print(predictions)
+print(prediction, f'{percentprob}% of having Diabetes')
